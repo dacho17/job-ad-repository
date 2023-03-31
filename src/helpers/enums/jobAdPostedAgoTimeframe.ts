3c4f30ba6afbb69780811959c9d4779b429210c3
dacho <dosenovicdavid7@gmail.com>
2023-03-31 02:51:52 +0200

    JobAdPostedAgoTimeframe Enum Added

---
+++ src/helpers/enums/jobAdPostedAgoTimeframe.ts
@@ -0,0 +1,12 @@
+export enum JobAdPostedAgoTimeframe {
+    UNKNOWN = 'UNKNOWN',
+    
+    NOW = 'now',
+    TODAY = 'today',
+    YESTERDAY = 'yesterday',
+    MINUTE = 'minute',
+    HOUR = 'hour',
+    DAY = 'day',
+    WEEK = 'week',
+    MONTH = 'month',    
+}
