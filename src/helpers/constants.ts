097cf64cdad357a63e25774f9b2cfbadcaf8af0d
dacho <dosenovicdavid7@gmail.com>
2023-03-31 02:55:42 +0200

    Utils, Constants and Packages to Support Job Ad Scraping Functionality

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,26 @@
 export default {
+    // jobsite urls
+    CAREER_BUILDER_URL: 'https://www.careerbuilder.com',
+    CAREER_JET_URL: 'https://www.careerjet.com',
+    CV_LIBRARY_URL: 'https://www.cv-library.co.uk',
+    EURO_ENGINEERING_URL: 'https://www.euroengineerjobs.com',
+    EURO_SCIENCE_URL: 'https://www.eurosciencejobs.com',
+    EURO_SPACE_CAREERS_URL: 'https://www.space-careers.com',
+    EURO_TECH_URL: 'https://www.eurotechjobs.com',
+    GRADUATELAND_URL: 'https://graduateland.com',
+    INDEED_URL: 'https://www.indeed.com',
+    JOB_FLUENT_URL: 'https://www.jobfluent.com',
+    JOBS_IN_NETWORK_URL: 'https://www.jobsinnetwork.com',
+    NO_FLUFF_JOBS_URL: 'https://nofluffjobs.com',
+    QREER_URL: 'https://www.qreer.com',
+    SIMPLY_HIRED_URL: 'https://www.simplyhired.com',
+    TYBA_URL: 'https://tyba.com',
+    WE_WORK_REMOTELY_URL: 'https://weworkremotely.com',
+
     // Commonly used strings
+    COMMA_SIGN: ',',
+    MINUS_SIGN: '-',
+    PLUS_SIGN: '+',
     EMPTY_STRING: '',
     WHITESPACE: ' ',
     WHITESPACE_URL_ENCODING: '%20',
@@ -14,12 +35,35 @@ export default {
 
     // common html selectors
     HREF_SELECTOR: 'href',
+    ARIALABEL_SELECTOR: 'aria-label',
 
     // joblinks selectors
     ADZUNA_JOBLINKS_SELECTOR: '.ui-search-results > div[data-aid] > .w-full > .flex.gap-4 > h2 > a',
+    ARBEITNOW_JOBLINKS_SELECTOR: '#results > li > div > .items-center > div > a',
+    CAREER_JET_JOBLINKS_SELECTOR: '.job.clicky > header > h2 > a',
+    CV_LIBRARY_JOBLINKS_SELECTOR: '#searchResults > .results__item > article > div > h2 > a',
+    EURES_JOBLINKS_SELECTOR: '.ecl-link--standalone',
+    EURO_JOBS_JOBLINKS_SELECTOR: '.viewDetails > a',
+    GRADUATELAND_JOBLINKS_SELECTOR: '.job-box',
+    INDEED_JOBLINKS_SELECTOR: 'h2.jobTitle > a',
+    JOB_FLUENT_JOBLINKS_SELECTOR: '.offer-title > a',
+    LN_JOBLINKS_SELECTOR: '.base-card__full-link',
+    NO_FLUFF_JOBS_JOBLINKS_SELECTOR: '.list-container > .posting-list-item',
+    QREER_JOBLINKS_SELECTOR: '.jobs-list > ul > li > .job',
+    SIMPLY_HIRED_JOBLINKS_SELECTOR: 'h3[data-testid="searchSerpJobTitle"] > a',
+    TYBA_JOBLINKS_SELECTOR: '#timeline > .section-view-list > .bem-enabled > a',
+    WE_WORK_REMOTELY_JOBLINKS_SELECTOR: 'li > a',
+    WE_WORK_REMOTELY_JOBLINKS_SELECTOR_TWO: '.jobs > article > ul > li > a',
+
+    // selectors used in job ad scraping
+    JOB_FLUENT_PUBLISHED_AGO_SELECTOR: '.published-date',
+    SIMPLY_HIRED_NAVIGATION_BUTTONS_SELECTOR: 'nav[role="navigation"] > a',
 
     // HTTP codes
     OK: 200,
     BAD_REQUEST: 400,
     SERVER_ERROR: 500,
+
+    // other
+    CV_LIBRARY_JOBLINK_SUFFIX: '-jobs',
 }
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,7 @@
 import { Service } from "typedi";
+import { addMinutes, addHours, addDays, addWeeks, addMonths} from 'date-fns';
+import Constants from "./constants";
+import { JobAdPostedAgoTimeframe } from "./enums/jobAdPostedAgoTimeframe";
 import { JobAdSource } from "./enums/jobAdSource";
 
 @Service()
@@ -8,5 +11,18 @@ export default class Utils {
         if (isNaN(timestamp)) return Date.parse((new Date().toString()));
 
         return timestamp;
+    }
+
+    // TODO: write the doc for the method. Needs to contain the description of the parameter received (expected format)
+    public getPostedDate4JobFluent(textContainingPostedAgo: string): Date {
+        const [_, firstPart, secondPart] = textContainingPostedAgo.trim().split(Constants.WHITESPACE);
+
+        if (firstPart.includes(JobAdPostedAgoTimeframe.YESTERDAY)) {
+            return addDays(Date.now(), -1);
+        } else if (secondPart.includes(JobAdPostedAgoTimeframe.DAY)) {
+            return addDays(Date.now(), -parseInt(firstPart));
+        } else {
+            return new Date();
+        }
     }
 }
