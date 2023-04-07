e13f6e8d5ab6446a664ccd91bf127212e6d1bbf5
dacho <dosenovicdavid7@gmail.com>
2023-04-07 11:02:37 +0200

    Job Parser Registered in JobParserHelper

--- src/services/jobParserHelper.ts
+++ src/services/jobParserHelper.ts
@@ -2,19 +2,35 @@ import { Inject, InjectMany, Service } from "typedi";
 import IJobParser from "../dataLayer/interfaces/IJobParser";
 import ArbeitNowParser from "../dataLayer/parsers/arbeitNowJobParser";
 import CareerBuilderJobParser from "../dataLayer/parsers/careerBuilderJobParser";
+import CareerJetJobParser from "../dataLayer/parsers/careerJetJobParser";
+import CvLibraryJobParser from "../dataLayer/parsers/cvLibraryJobParser";
+import SimplyhiredJobParser from "../dataLayer/parsers/simplyHiredJobParser";
+import WeWorkRemotelyJobParser from "../dataLayer/parsers/weWorkRemotelyJobParser";
 import { JobAdSource } from "../helpers/enums/jobAdSource";
 
 @Service()
 export default class JobParserHelper {
     private arbeitNowParser: ArbeitNowParser;
     private careerBuilderParser: CareerBuilderJobParser;
+    private careerJetParser: CareerJetJobParser;
+    private cvLibraryParser: CvLibraryJobParser;
+    private simplyHiredParser: SimplyhiredJobParser;
+    private weWorkRemotelyParser: WeWorkRemotelyJobParser;
 
     constructor(
         @Inject() arbeitNowParser: ArbeitNowParser,
         @Inject() careerBuilderParser: CareerBuilderJobParser,
+        @Inject() careerJetParser: CareerJetJobParser,
+        @Inject() cvLibraryParser: CvLibraryJobParser,
+        @Inject() simplyHiredParser: SimplyhiredJobParser,
+        @Inject() weWorkRemotelyParser: WeWorkRemotelyJobParser,
     ) {
         this.arbeitNowParser = arbeitNowParser;
         this.careerBuilderParser = careerBuilderParser;
+        this.careerJetParser = careerJetParser;
+        this.cvLibraryParser = cvLibraryParser;
+        this.simplyHiredParser = simplyHiredParser;
+        this.weWorkRemotelyParser = weWorkRemotelyParser;
     }
 
     /**
@@ -24,39 +40,18 @@ export default class JobParserHelper {
    */
     public getParserFor(jobAdSource: JobAdSource): IJobParser | null {
         switch (jobAdSource) {
-            // case JobAdSource.ADZUNA:
-            //     return this.adzunaScraper;
             case JobAdSource.ARBEIT_NOW:
                 return this.arbeitNowParser;
             case JobAdSource.CAREER_BUILDER:
                 return this.careerBuilderParser;
-            // case JobAdSource.CAREER_JET:
-            //     return this.careerJetScraper;
-            // case JobAdSource.CV_LIBRARY:
-            //     return this.cvLibraryScraper;
-            // case JobAdSource.EURO_JOBS:
-            //     return this.euroJobScraper;
-            // case JobAdSource.EURO_ENGINEER_JOBS:
-            // case JobAdSource.EURO_SCIENCE_JOBS:
-            // case JobAdSource.EURO_SPACE_CAREERS:
-            // case JobAdSource.EURO_TECH_JOBS:
-            //     return this.euroJobSitesScraper;
-            // case JobAdSource.GRADUATELAND:
-            //     return this.graduatelandScraper;
-            // case JobAdSource.JOB_FLUENT:
-            //     return this.jobFluentScraper;
-            // case JobAdSource.LINKEDIN:
-            //     return this.linkedinScraper;
-            // case JobAdSource.NO_FLUFF_JOBS:
-            //     return this.noFluffScraper;
-            // case JobAdSource.QREER:
-            //     return this.qreerScraper;
-            // case JobAdSource.SIMPLY_HIRED:
-            //     return this.simplyHiredScraper;
-            // case JobAdSource.TYBA:
-            //     return this.tybaScraper;
-            // case JobAdSource.WE_WORK_REMOTELY:
-            //     return this.weWorkRemotely;
+            case JobAdSource.CAREER_JET:
+                return this.careerJetParser;
+            case JobAdSource.CV_LIBRARY:
+                return this.cvLibraryParser;
+            case JobAdSource.SIMPLY_HIRED:
+                return this.simplyHiredParser;
+            case JobAdSource.WE_WORK_REMOTELY:
+                return this.weWorkRemotelyParser;            
             default:
                 return null;
         }
