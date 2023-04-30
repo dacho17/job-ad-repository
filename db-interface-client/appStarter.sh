1ac2f9064618215c01faf4ee80589d84c2ad268c
dacho <dosenovicdavid7@gmail.com>
2023-04-30 14:07:46 +0200

    react-script Execution moved to package.json

--- db-interface-client/appStarter.sh
+++ db-interface-client/appStarter.sh
@@ -3,7 +3,7 @@ npm install
 
 # export NODE_OPTIONS="--max_old_space_size=448"
 
-react-scripts --optimize_for_size --max_old_space_size=480 --gc_interval=100 start
+npm run start
 
 # npm run build
 
